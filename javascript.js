e98c5fd9447249a2ed68f5d57e6b18aab0f1566f
Rodrigo Passos <rodrigoarapassos@gmail.com>
2022-08-31 20:12:01 -0300

    Add button start, grid working
    Next add win condition and logic that keeps players from playing in spots
    already taken.

--- javascript.js
+++ javascript.js
@@ -7,4 +7,56 @@ const Player = sign => {
 const gameBoard = (() => {
     let gb = ["", "", "", "", "", "", "", "", ""];
     
+    const gameMode = (mode) => {
+        let playing = "playerX";
+        if (mode == "pvp") {
+            const playerX = Player("X");
+            const playerO = Player("O");
+            document.querySelectorAll(".gb").forEach((boardCell) => {
+                boardCell.addEventListener("click", () => {
+                    let index = Number(boardCell.getAttribute("data-cell"));
+                    if (playing == "playerX") {
+                        play(playerX, index);
+                        playing = "playerO";
+                    }else {
+                        play(playerO, index);
+                        playing = "playerX";
+                    }
+                })
+                
+            })
+            
+        }
+    }
+
+    const play = (player, index) => {
+                let playerSign = player.getSign();
+                gb[index] = playerSign;
+                console.log(playerSign);
+                display.write();
+    }
+        
+
+    return {gb, gameMode};
 })();
+
+const display = (() => {
+    const write = () => {
+        const board = document.querySelectorAll(".gb");
+        board.forEach((cell) => {
+            cell.innerHTML = gameBoard.gb[cell.getAttribute("data-cell")];
+        })
+    }
+    return {write};
+})();
+
+const start = (() => {
+    document.querySelector(".PvP").addEventListener("click", () => {
+        for (let i = 0; i < gameBoard.gb.length; i++){
+            gameBoard.gb[i] = "";
+        }
+        display.write();
+        gameBoard.gameMode("pvp");
+    })
+})();
+
