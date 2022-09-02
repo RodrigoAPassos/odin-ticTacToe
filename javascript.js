edc6cbd3865ea38a3cefd6d516b653de3639ac00
Rodrigo Passos <rodrigoarapassos@gmail.com>
2022-09-02 18:01:26 -0300

    Rework the js code to work correctly
    Next finish the on win state, add a reset button and finish the style

--- javascript.js
+++ javascript.js
@@ -1,124 +1,96 @@
-const Player = sign => {
+const Player = (sign) => {
     let score = 0;
     const getSign = () => sign;
-    return {getSign, score};
+    
+    return {score, getSign};
 }
 
 const gameBoard = (() => {
-    let gb = ["", "", "", "", "", "", "", "", ""];
-    let round = 0;
+    const gb = ["", "", "", "", "", "", "", "", ""];
     const playerX = Player("X");
     const playerO = Player("O");
+    let playing = playerX;
     
+    return {gb, playing, playerX, playerO, ready};
+})();
+
+const game = (() => {
     const gameMode = (mode) => {
-        let playing = "playerX";
-        round = 0;
         if (mode == "pvp") {
-            document.querySelectorAll(".gb").forEach((boardCell) => {
-                boardCell.addEventListener("click", () => {
-                    let index = Number(boardCell.getAttribute("data-cell"));
-                    console.log(playing);
-                    if (boardCell.innerHTML == "") {
-                        if (playing == "playerX") {
-                            play(playerX, index);
-                            playing = "playerO";
-                        }else {
-                            play(playerO, index);
-                            playing = "playerX";
-                        }
-                    } else return;
+            display.clear();
+
+            document.querySelectorAll(".gb").forEach((cell) => {
+                cell.addEventListener("click", () => {
+                    let index = cell.getAttribute("data-cell");
+                    play(index);
                 })
-                
             })
-              
+
+        }else return;
+    }
+
+    const changeTurn = () => {
+        if (gameBoard.playing == gameBoard.playerX) {
+            gameBoard.playing = gameBoard.playerO;
+        }else gameBoard.playing = gameBoard.playerX;
+    }
+
+    const play = (index) => {
+        if (gameBoard.gb[index] == "") {
+            gameBoard.gb[index] = gameBoard.playing.getSign();
+            display.write();
+            checkwinner();
+            changeTurn();
+        }else return;
+    }
+
+    const checkwinner = () => {
+        let checkTie = gameBoard.gb.filter(elem => elem == "X" || elem == "O");
+        if (gameBoard.gb[0] === gameBoard.gb[1] && gameBoard.gb[0] === gameBoard.gb[2] && gameBoard.gb[0] != ""){
+            winner();
+            console.log(gameBoard.playing.getSign(), gameBoard.playing.score);
+        }else if (gameBoard.gb[3] === gameBoard.gb[4] && gameBoard.gb[3] === gameBoard.gb[5] && gameBoard.gb[3] != ""){
+            console.log("Win");
+        }else if (gameBoard.gb[6] === gameBoard.gb[7] && gameBoard.gb[6] === gameBoard.gb[8] && gameBoard.gb[6] != ""){
+            console.log("Win");
+        }else if (gameBoard.gb[0] === gameBoard.gb[3] && gameBoard.gb[0] === gameBoard.gb[6] && gameBoard.gb[0] != ""){
+            console.log("Win");
+        }else if (gameBoard.gb[1] === gameBoard.gb[4] && gameBoard.gb[1] === gameBoard.gb[7] && gameBoard.gb[1] != ""){
+            console.log("Win");
+        }else if (gameBoard.gb[2] === gameBoard.gb[5] && gameBoard.gb[2] === gameBoard.gb[8] && gameBoard.gb[2] != ""){
+            console.log("Win");
+        }else if (gameBoard.gb[0] === gameBoard.gb[4] && gameBoard.gb[0] === gameBoard.gb[8] && gameBoard.gb[0] != ""){
+            console.log("Win");
+        }else if (gameBoard.gb[2] === gameBoard.gb[4] && gameBoard.gb[2] === gameBoard.gb[6] && gameBoard.gb[2] != ""){
+            console.log("Win");
+        }else if (checkTie.length == 9){
+            console.log("TIE!");
         }
     }
 
-    const checkWinner = (player, round) => {
-        let playerSign = player.getSign();
-        if (gb[0] === gb[1] && gb[0] === gb[2] && gb[0] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[3] === gb[4] && gb[3] === gb[5] && gb[3] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[6] === gb[7] && gb[6] === gb[8] && gb[6] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[0] === gb[3] && gb[0] === gb[6] && gb[0] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[1] === gb[4] && gb[1] === gb[7] && gb[1] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[2] === gb[5] && gb[2] === gb[8] && gb[2] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[0] === gb[4] && gb[0] === gb[8] && gb[0] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (gb[2] === gb[4] && gb[2] === gb[6] && gb[2] != ""){
-            console.log(`Player ${playerSign} wins!`);
-            player.score++;
-            console.log(`Player ${playerSign} has ${player.score} wins!`);
-            winner(player);
-        }else if (round > 8){
-            console.log("Tie!");
-        }
+    const winner = () => {
+        gameBoard.playing.score++;
+        document.querySelector(".message").innerHTML = `Player ${gameBoard.playing.getSign()} Won!`
     }
 
-    const winner = (player) => {
-        for (let i = 0; i < gameBoard.gb.length; i++){
-            gameBoard.gb[i] = "";
-        }
-        display.write();
-        gameMode("pvp");
-    }
-
-    const play = (player, index) => {
-        let playerSign = player.getSign();
-        gb[index] = playerSign;
-        display.write();
-        round++;
-        checkWinner(player, round);
-                
-    }
-        
-
-    return {gb, gameMode};
+    return{gameMode};
 })();
 
 const display = (() => {
+
     const write = () => {
-        const board = document.querySelectorAll(".gb");
-        board.forEach((cell) => {
+        document.querySelectorAll(".gb").forEach((cell) => {
             cell.innerHTML = gameBoard.gb[cell.getAttribute("data-cell")];
         })
     }
-    return {write};
-})();
 
-const start = (() => {
-    document.querySelector(".PvP").addEventListener("click", () => {
-        for (let i = 0; i < gameBoard.gb.length; i++){
+    const clear = () => {
+        for(let i = 0; i < gameBoard.gb.length; i++) {
             gameBoard.gb[i] = "";
         }
-        display.write();
-        gameBoard.gameMode("pvp");
-    })
-})();
+        gameBoard.playing = gameBoard.playerX;
+        write();
+    }
 
+    return {write, clear};
+})();
