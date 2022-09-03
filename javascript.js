f7f3c692bb995834860efcfe8058494d24628fcd
Rodrigo Passos <rodrigoarapassos@gmail.com>
2022-09-03 19:03:36 -0300

    Add reset button and finished win state
    Next finish style the page

--- javascript.js
+++ javascript.js
@@ -11,15 +11,37 @@ const gameBoard = (() => {
     const playerO = Player("O");
     let playing = playerX;
     
-    return {gb, playing, playerX, playerO, ready};
+    return {gb, playing, playerX, playerO};
 })();
 
 const game = (() => {
+    let round = 0;
     const gameMode = (mode) => {
         if (mode == "pvp") {
             display.clear();
+            round++;
+            document.querySelector(".message").innerHTML = `Round ${round}`
+            
 
             document.querySelectorAll(".gb").forEach((cell) => {
+                cell.classList.remove("disabled");
+                cell.addEventListener("click", () => {
+                    let index = cell.getAttribute("data-cell");
+                    play(index);
+                })
+            })
+
+        }else if (mode == "reset") {
+            display.clear();
+            round = 0;
+            gameBoard.playerX.score = 0;
+            gameBoard.playerO.score = 0;
+            document.querySelector(".score").innerHTML = `Player ${gameBoard.playerX.getSign()}: ${gameBoard.playerX.score} &#x2694 Player ${gameBoard.playerO.getSign()}: ${gameBoard.playerO.score}`
+
+            document.querySelector(".message").innerHTML = `Round ${round}`
+            
+            document.querySelectorAll(".gb").forEach((cell) => {
+                cell.classList.remove("disabled");
                 cell.addEventListener("click", () => {
                     let index = cell.getAttribute("data-cell");
                     play(index);
@@ -48,29 +70,32 @@ const game = (() => {
         let checkTie = gameBoard.gb.filter(elem => elem == "X" || elem == "O");
         if (gameBoard.gb[0] === gameBoard.gb[1] && gameBoard.gb[0] === gameBoard.gb[2] && gameBoard.gb[0] != ""){
             winner();
-            console.log(gameBoard.playing.getSign(), gameBoard.playing.score);
         }else if (gameBoard.gb[3] === gameBoard.gb[4] && gameBoard.gb[3] === gameBoard.gb[5] && gameBoard.gb[3] != ""){
-            console.log("Win");
+            winner();
         }else if (gameBoard.gb[6] === gameBoard.gb[7] && gameBoard.gb[6] === gameBoard.gb[8] && gameBoard.gb[6] != ""){
-            console.log("Win");
+            winner();
         }else if (gameBoard.gb[0] === gameBoard.gb[3] && gameBoard.gb[0] === gameBoard.gb[6] && gameBoard.gb[0] != ""){
-            console.log("Win");
+            winner();
         }else if (gameBoard.gb[1] === gameBoard.gb[4] && gameBoard.gb[1] === gameBoard.gb[7] && gameBoard.gb[1] != ""){
-            console.log("Win");
+            winner();
         }else if (gameBoard.gb[2] === gameBoard.gb[5] && gameBoard.gb[2] === gameBoard.gb[8] && gameBoard.gb[2] != ""){
-            console.log("Win");
+            winner();
         }else if (gameBoard.gb[0] === gameBoard.gb[4] && gameBoard.gb[0] === gameBoard.gb[8] && gameBoard.gb[0] != ""){
-            console.log("Win");
+            winner();
         }else if (gameBoard.gb[2] === gameBoard.gb[4] && gameBoard.gb[2] === gameBoard.gb[6] && gameBoard.gb[2] != ""){
-            console.log("Win");
+            winner();
         }else if (checkTie.length == 9){
-            console.log("TIE!");
+            document.querySelector(".message").innerHTML = "Tie!"
         }
     }
 
     const winner = () => {
         gameBoard.playing.score++;
-        document.querySelector(".message").innerHTML = `Player ${gameBoard.playing.getSign()} Won!`
+        document.querySelector(".message").innerHTML = `Player ${gameBoard.playing.getSign()} Won! Click on PvP button to rematch or reset button to erase score...`
+        document.querySelector(".score").innerHTML = `Player ${gameBoard.playerX.getSign()}: ${gameBoard.playerX.score} &#x2694 Player ${gameBoard.playerO.getSign()}: ${gameBoard.playerO.score}`
+        document.querySelectorAll(".gb").forEach((cell) => {
+            cell.classList.add("disabled");
+        })
     }
 
     return{gameMode};
